1c81b2c06c45cdcd3a475d5ae36a7a460ea05774
Lucas Testard <lucastestard77@gmail.com>
2026-06-22 14:35:15 +0200

    fix: media files in prod

--- mmotors/urls.py
+++ mmotors/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenRefreshView
 from dossiers.urls import contrat_urlpatterns, sav_urlpatterns
@@ -35,7 +36,7 @@ urlpatterns = [
     path('api/dossiers/', include('dossiers.urls')),
     path('api/contrats/', include(contrat_urlpatterns)),
     path('api/sav/',      include(sav_urlpatterns)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
 
 admin.site.site_header = 'M-Motors — Administration'
 admin.site.site_title  = 'M-Motors'
